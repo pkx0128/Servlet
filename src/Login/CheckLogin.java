0d42141be42c15c6a1ae11c76895cb82c25b2626
pankx <764670547@qq.com>
2019-08-24 16:52:57 +0800

    连接到数据库验证账号

--- src/Login/CheckLogin.java
+++ src/Login/CheckLogin.java
@@ -1,12 +1,19 @@
 package Login;
 
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import javax.servlet.ServletException;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 
 
 public class CheckLogin extends HttpServlet {
@@ -17,24 +24,53 @@ public class CheckLogin extends HttpServlet {
 		resp.setContentType("text/html;charset=utf-8");
 		String username = res.getParameter("username");
 		String password = res.getParameter("password");
-//		判断指定用户名和密码
-		if(username.equals("abc") && password.equals("123456")) {
-//			保存登录用户名到Session
-			session.setAttribute("username", username);
-//			保存登录密码到Session
-			session.setAttribute("password", password);
-//			设置Session有效时间为30秒
-			session.setMaxInactiveInterval(30);
+		
+		try {
+//		加载数据库驱动
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/spdb","root","Pankx0128");
+			String sql = "SELECT username,passwd FROM users WHERE username = ?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setString(1, username);
+			ResultSet re = ps.executeQuery();
+			System.out.println("Sql 开始");
+			System.out.println("验证账号");
+//			判断指定用户名和密码
+//			if(username.equals(re.getString("username")) && password.equals(re.getString("passwd"))) {
+			if(re.next()) {
+//				保存登录用户名到Session
+				session.setAttribute("username", username);
+//				保存登录密码到Session
+				session.setAttribute("password", password);
+//				设置Session有效时间为30秒
+				session.setMaxInactiveInterval(30);
+				
+				System.out.println("登录成功。。。。");
+//				跳转到Welcome页面并且传递username的值
+//				resp.sendRedirect("welcome？username="+username+"&password="+password);
+//				跳转到Welcome页面
+				resp.sendRedirect("welcome");
+			}else {
+				System.out.println("登录失败。。。。");
+				resp.sendRedirect("login");
+			}
+			conn.close();
 			
-			System.out.println("登录成功。。。。");
-//			跳转到Welcome页面并且传递username的值
-//			resp.sendRedirect("welcome？username="+username+"&password="+password);
-//			跳转到Welcome页面
-			resp.sendRedirect("welcome");
-		}else {
-			System.out.println("登录失败。。。。");
-			resp.sendRedirect("login");
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+		
+		
+
 	}
 	
 	public void doPost(HttpServletRequest res,HttpServletResponse resp) throws ServletException,IOException{
